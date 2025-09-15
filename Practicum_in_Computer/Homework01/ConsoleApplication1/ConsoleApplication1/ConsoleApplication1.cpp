69a240cd925769b6e51eeeed3cf630cf6a52171d
taitaiwu <s919075@stu.chsc.tw>
2025-09-15 20:13:09 +0800

    C_sharp update

---
+++ Practicum_in_Computer/Homework01/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    char name[10];
+    char department;
+    int score1, score2;
+    float avg;
+
+    printf("Please input your name: ");
+    scanf("%s", name);
+
+    printf("Please select your department (a:資工 b:電機 c:電子): ");
+    scanf(" %c", &department);
+
+    printf("Please input your score on test 1: ");
+    scanf("%d", &score1);
+
+    printf("Please input your score on test 2: ");
+    scanf("%d", &score2);
+
+    avg = ((float)score1 + (float)score2) / 2;
+
+    printf("|-----------------------------------------------------|\n");
+    printf("|    Name    | Department | Test 1 | Test 2 | Average |\n");
+    printf("|------------+------------+--------+--------+---------|\n");
+    printf("|%-10s  |%-10c  | %6d | %6d |%8.2f |\n", name, department, score1, score2, avg);
+    printf("|-----------------------------------------------------|\n");
+
+}
---
+++ Practicum_in_Computer/Homework01/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	printf("Please enter a positiveinteger:");
+	scanf("%d", &a);
+
+	if (a < 0) printf("%d is not a positive integer!!\n", a);
+	else if (a > 0 && a < 10) printf("%d is a small integer.", a);
+	else if (a > 9 && a < 100) printf("%d的十位數為%d\n%d的個位數為%d", a, a / 10, a, a % 10);
+	else if (a > 99 && a % 5 == 0) printf("%d is a large integer.\n%d is divisible by 5.", a, a);
+	else if (a > 99 && a % 5 == 1) printf("%d is a large integer.\n%d+5=%d", a, a, a + 5);
+	else if (a > 99 && a % 5 == 2) printf("%d is a large integer.\n%d-5=%d", a, a, a - 5);
+	else if (a > 99 && a % 5 == 3) printf("%d is a large integer.\n%d*5=%d", a, a, a * 5);
+	else if (a > 99 && a % 5 == 4) printf("%d is a large integer.\n%d/5=%.2f", a, a, (float)a / 5);
+}
